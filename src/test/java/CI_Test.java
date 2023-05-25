0a719c5166189534418851af1d4ca3e3e128562c
kofinandi <kofarago.nandi@gmail.com>
2023-05-25 16:53:11 +0200

    unit test added

---
+++ src/test/java/CI_Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CI_Test {
+    @Test
+    public void testSomeMethod() {
+        // Assert
+        assertEquals(5, 5);
+    }
+}
