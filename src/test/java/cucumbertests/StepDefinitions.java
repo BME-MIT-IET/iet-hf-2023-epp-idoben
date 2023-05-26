ad884ab52fcec6dad3d17d11471cfdf2d1c6331b
kofinandi <kofarago.nandi@gmail.com>
2023-05-26 14:16:17 +0200

    added more cucumber tests #4

--- src/test/java/cucumbertests/StepDefinitions.java
+++ src/test/java/cucumbertests/StepDefinitions.java
@@ -1,35 +1,60 @@
 package cucumbertests;
 
-import agent.FullProt;
 import com.main.Virologist;
+import equipment.Axe;
+import equipment.Gloves;
 import equipment.ProtSuit;
 import equipment.Sack;
+import field.Field;
 import field.SafeHouse;
 import field.Simple;
+import field.WareHouse;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-import java.util.HashSet;
+import resources.Aminoacid;
 
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
     static Virologist v1;
     static Simple simple;
-    static SafeHouse safeHouse;
+    static SafeHouse safeHouse1;
+    static SafeHouse safeHouse2;
+    static SafeHouse safeHouse3;
     static ProtSuit p1;
+    static ProtSuit p2;
     static Sack s1;
+    static Gloves g1;
+    static Axe a1;
 
     @Given("a virologist and a safe house with a ProtSuit")
-    public void virologistandsafehouse(){
+    public void virologistandsafehouse() {
         v1 = new Virologist("Virologist");
         p1 = new ProtSuit(5, null, "protsuit p1");
-        safeHouse = new SafeHouse("safeHouse", p1, 1, 2);
+        safeHouse1 = new SafeHouse("safeHouse1", p1, 1, 2);
         simple = new Simple("simple", 3, 4);
-        safeHouse.AddNeighbour("0",simple);
-        simple.AddNeighbour("0", safeHouse);
+        safeHouse1.AddNeighbour("0",simple);
+        simple.AddNeighbour("0", safeHouse1);
+        simple.Accept(v1);
     }
+
+    @Given("a safe house with an Axe")
+    public void safehousewithanaxe() {
+        a1 = new Axe(null, "axe a1");
+        safeHouse2 = new SafeHouse("safeHouse2", a1, 1, 3);
+        safeHouse1.AddNeighbour("1", safeHouse2);
+        safeHouse2.AddNeighbour("1", safeHouse1);
+    }
+
+    @Given("a safe house with a ProtSuit")
+    public void safehousewithaprotsuit() {
+        p2 = new ProtSuit(5, null, "protsuit p2");
+        safeHouse3 = new SafeHouse("safeHouse2", p2, 1, 4);
+        safeHouse2.AddNeighbour("0", safeHouse3);
+        safeHouse3.AddNeighbour("0", safeHouse2);
+    }
+
 
     @Given("the virologist has a Sack")
     public void virologisthassack() {
@@ -37,27 +62,47 @@ public class StepDefinitions {
         v1.PickupEq(s1);
     }
 
-    @When("the virologist steps on the safehouse")
+    @Given("the virologist has a Glove")
+    public void virologisthasgloves() {
+        g1 = new Gloves(5, 5, null, "gloves g1");
+        v1.PickupEq(g1);
+    }
+
+    @When("the virologist steps on the safehouse with a ProtSuit")
     public void step1() {
-        safeHouse.Accept(v1);
-        v1.Accept(safeHouse, new HashSet<Virologist>());
         v1.Move("0");
     }
 
-    @When("the virologist collects the ProtSuit")
+    @When("the virologist collects the equipment from the field")
     public void collect1() {
-        safeHouse.Collect(v1);
+        v1.Collect();
+    }
+
+    @When("the virologist steps on the safehouse with an Axe")
+    public void step2() {
+        v1.Move("1");
     }
 
     @Then("the virologist is in the safe house")
     public void virologistinsafehouse() {
-        assertTrue(simple.IsHere(v1));
+        assertTrue(safeHouse1.IsHere(v1));
     }
 
-    @Then("the virologist is not on the previous field")
+    @Then("the virologist is not on the first field")
     public void virologistnotonfield() {
-        assertFalse(safeHouse.IsHere(v1));
+        assertFalse(simple.IsHere(v1));
     }
+
+    @Then("the virologist is not on the safe house with ProtSuit")
+    public void virologistnotonsafehouse() {
+        assertFalse(safeHouse1.IsHere(v1));
+    }
+
+    @Then("the virologist is in the safe house with Axe")
+    public void virologistinsafehousewithaxe() {
+        assertTrue(safeHouse2.IsHere(v1));
+    }
+
 
     @Then("the virologist has ProtSuit")
     public void virologisthasprotsuit() {
--- src/test/resources/cucumbertests/cucumber.feature
+++ src/test/resources/cucumbertests/cucumber.feature
@@ -1,32 +1,53 @@
-Feature: Virologist stepping
+Feature: Virologist stepping and collecting equipment
 
   Scenario: Virologist steps on safehouse
     Given a virologist and a safe house with a ProtSuit
-    When the virologist steps on the safehouse
+    When the virologist steps on the safehouse with a ProtSuit
     Then the virologist is in the safe house
-    And the virologist is not on the previous field
+    And the virologist is not on the first field
 
   Scenario: Virologist collects ProtSuit
     Given a virologist and a safe house with a ProtSuit
-    When the virologist steps on the safehouse
-    And the virologist collects the ProtSuit
+    When the virologist steps on the safehouse with a ProtSuit
+    And the virologist collects the equipment from the field
     Then the virologist has ProtSuit
 
   Scenario: Virologist steps but does not collect ProtSuit
     Given a virologist and a safe house with a ProtSuit
-    When the virologist steps on the safehouse
+    When the virologist steps on the safehouse with a ProtSuit
     Then the virologist does not have equipment
 
   Scenario: A Virologist steps on the safe house and collects the ProtSuit
     Given a virologist and a safe house with a ProtSuit
     And the virologist has a Sack
-    When the virologist steps on the safehouse
-    And the virologist collects the ProtSuit
+    When the virologist steps on the safehouse with a ProtSuit
+    And the virologist collects the equipment from the field
     Then the virologist has 2 equipments
 
   Scenario: A Virologist steps on the safe house but does not collect ProtSuit
     Given a virologist and a safe house with a ProtSuit
     And the virologist has a Sack
-    When the virologist steps on the safehouse
+    When the virologist steps on the safehouse with a ProtSuit
     Then the virologist has 1 equipments
 
+  Scenario: A Virologist tries to collect too many equipments
+    Given a virologist and a safe house with a ProtSuit
+    And a safe house with an Axe
+    And a safe house with a ProtSuit
+    And the virologist has a Glove
+    When the virologist steps on the safehouse with a ProtSuit
+    And the virologist collects the equipment from the field
+    And the virologist steps on the safehouse with an Axe
+    And the virologist collects the equipment from the field
+    And the virologist steps on the safehouse with a ProtSuit
+    And the virologist collects the equipment from the field
+    Then the virologist has 3 equipments
+
+  Scenario: A Virologist steps multiple times
+    Given a virologist and a safe house with a ProtSuit
+    And a safe house with an Axe
+    When the virologist steps on the safehouse with a ProtSuit
+    And the virologist steps on the safehouse with an Axe
+    Then the virologist is not on the first field
+    And the virologist is not on the safe house with ProtSuit
+    And the virologist is in the safe house with Axe
